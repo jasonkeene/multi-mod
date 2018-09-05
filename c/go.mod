ff17a1473db955212f76bfd2b79e14529a7735e8
Jason Keene <jkeene@pivotal.io>
2018-09-05 17:21:48 -0600

    add c

---
+++ c/go.mod
@@ -0,0 +1,6 @@
+module github.com/jasonkeene/multi-mod/c
+
+require (
+	github.com/jasonkeene/multi-mod/a v1.1.0
+	github.com/jasonkeene/multi-mod/b v1.6.3
+)
---
+++ c/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jasonkeene/multi-mod/a"
+	"github.com/jasonkeene/multi-mod/b"
+)
+
+func main() {
+	fmt.Println(a.A + " " + b.B)
+}
